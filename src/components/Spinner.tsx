3a7e5873b5bc78d3106e91ec0684967362293acc
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-08-29 19:39:10 +0530

    some change

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { FiLoader } from 'react-icons/fi';
 
 const Spinner = () => (
-  <div className="spinner-container d-flex justify-content-center align-items-center" style={{
-    height:"20px",
-    width:"20px"
-  }}>
+  <div className="spinner-container d-flex justify-content-center align-items-center">
     <FiLoader className="spinner-icon" />
   </div>
 );
